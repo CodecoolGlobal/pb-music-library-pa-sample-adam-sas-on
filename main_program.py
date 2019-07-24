871c207c5ea1587db689e7e58bdd91752e38d3e5
Adam Sz <adamsz303@gmail.com>
2019-07-24 13:59:56 +0200

    Make optional question choosable  and  swap spaces to tabs for easier edition.

--- main_program.py
+++ main_program.py
@@ -7,79 +7,92 @@ import curses
 
 
 def scroll_list(stdscr, lists):
-    pass
+	pass
 #
 
 
 def menu(stdscr, albums):
-    stdscr = curses.initscr()
+	stdscr = curses.initscr()
 
-    curses.noecho()
-    curses.cbreak()
-    stdscr.keypad(True)
+	curses.noecho()
+	curses.cbreak()
+	stdscr.keypad(True)
 
 
-    run = True
-    cmd = 0
+	run = True if len(albums) > 0 else False
+	cmd = 0
 
-    stdscr.addstr(0, 4, "Music library.")
-    options = ["Get albums by genre",
-				"Get longest album",
+	stdscr.addstr(0, 4, "Music library.")
+	options = ["Get longest album",
 				"Get total albums length",
 				"Get genre stats",
 				"Get last oldest",
-				"Get last oldest of genre"]
+				"Get last oldest of genre",
+				"Get albums by genre"]
+#TEST_QUESTION
+
+	genres = music_reports.get_genres(albums)
+	i4genres = 0
+
+	while run:
+		line_no = 2
+		if len(genres) > 0:
+			stdscr.addstr(line_no, 0, " {}: {}".format(options[0], genres[i4genres]) )
+			line_no += 1
+
+		i = 1
+		while i < len(options): # print all questions in the same way;
+			stdscr.addstr(line_no, 0, "{}) {}".format(i+1, options[i]) )
+			i += 1
+			line_no += 1
+		stdscr.addstr(i + 2, 0, " Exit")
+
+		if cmd == len(options):
+			stdscr.addstr(len(options) + 2, 0, " Exit", curses.A_BOLD)
+		else:
+			stdscr.addstr(cmd + 2, 0, "{}) {}".format(cmd + 1, options[cmd]), curses.A_BOLD)
 
 
-    while run:
-        i = 0
-        while i < len(options): # print all questions in the same way;
-            stdscr.addstr(i + 2, 0, "{}) {}".format(i+1, options[i]) )
-            i += 1
-        stdscr.addstr(i + 2, 0, " Exit")
+		c=stdscr.getch()
 
-        if cmd == len(options):
-            stdscr.addstr(len(options) + 2, 0, " Exit", curses.A_BOLD)
-        else:
-            stdscr.addstr(cmd + 2, 0, "{}) {}".format(cmd + 1, options[cmd]), curses.A_BOLD)
+		if c==curses.KEY_UP and cmd > 0:
+			cmd -= 1
+		elif c==curses.KEY_DOWN and cmd < len(options):
+			cmd += 1
+		elif c==curses.KEY_RIGHT and i4genres < len(genres)-1 and cmd == 0:
+			i4genres += 1
+		elif c==curses.KEY_RIGHT and i4genres > 0 and cmd == 0:
+			i4genres -= 1
+		elif (c==curses.KEY_ENTER or c==10) and cmd == 0:
+			pass
+		elif (c==curses.KEY_ENTER or c==10) and cmd == len(options):
+			run = False
 
+	#
 
-        c=stdscr.getch()
+	curses.nocbreak()
+	stdscr.keypad(False)
+	curses.echo()
 
-        if c==curses.KEY_UP and cmd > 0:
-            cmd -= 1
-        elif c==curses.KEY_DOWN and cmd < len(options):
-            cmd += 1
-        elif (c==curses.KEY_ENTER or c==10) and cmd == 0:
-
-        elif (c==curses.KEY_ENTER or c==10) and cmd == len(options):
-            run = False
-
-    #
-
-    curses.nocbreak()
-    stdscr.keypad(False)
-    curses.echo()
-
-    curses.endwin()
+	curses.endwin()
 #
 
 def main():
-    """
-    Calls all interaction between user and program, handles program menu
-    and user inputs. It should repeat displaying menu and asking for
-    input until that moment.
+	"""
+	Calls all interaction between user and program, handles program menu
+	and user inputs. It should repeat displaying menu and asking for
+	input until that moment.
 
-    You should create new functions and call them from main whenever it can
-    make the code cleaner
-    """
-    albums = file_handling.import_data()
+	You should create new functions and call them from main whenever it can
+	make the code cleaner
+	"""
+	albums = file_handling.import_data()
 
-    curses.wrapper(menu, albums)
+	curses.wrapper(menu, albums)
 
 
 #
 
 if __name__ == '__main__':
-    main()
+	main()
 
