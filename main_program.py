c6a525c7da46b3bb6bb4f01fce095841ec37f92e
Adam Sz <adamsz303@gmail.com>
2019-07-24 16:04:17 +0200

    Move all option elements and methods into one class.

--- main_program.py
+++ main_program.py
@@ -5,55 +5,95 @@ import file_handling
 import music_reports
 import curses
 
+class Options:
+	def __init__(self):
+		self.options = ["Exit"]
+		self.indexes = [0]
+		self.states = [None]
+		self.current = 0
+
+	def add(self, option, elements, index=0):
+		i = len(self.options) - 1
+
+		self.options[i] = option
+		if type(elements) == list and len(elements) > 0:
+			self.states[i] = elements
+			index2 = index if index < len(elements) else 0
+			self.indexes[i] = index2
+
+		self.options.append("Exit")
+		self.indexes.append(0)
+		self.states.append(None)
+	#
+	def is_bold(self, whch):
+		return True if whch == self.current else False
+	#
+	def scr_print(self, stdscr, first_line=2):
+		i = 0
+		line_no = first_line
+		while i < len(self.options):
+			if type(self.states[i]) == list:
+				opts = self.states[i]
+				index = self.indexes[i]
+				stdscr.addstr(line_no, 0, "- {}: {}".format(self.options[i], opts[index]) )
+			else:
+				stdscr.addstr(line_no, 0, "- {}".format(self.options[i]) )
+			i += 1
+			line_no += 1
+		#
+
+		# bold the whch's option;
+		line_no = first_line + self.current
+		if type(self.states[self.current]) == list:
+			opts = self.states[self.current]
+			index = self.indexes[self.current]
+			stdscr.addstr(line_no, 0, "- {}: {}".format(self.options[self.current], opts[index]), curses.A_BOLD)
+		else:
+			stdscr.addstr(line_no, 0, "- {}".format(self.options[self.current]), curses.A_BOLD)
+	#
+	def move_up(self):
+		if self.current > 0:
+			self.current -= 1
+	#
+	def move_down(self):
+		if self.current < len(self.options) - 1:
+			self.current += 1
+	#
+	def print_next_element(self, stdscr, first_line=2):
+		if type(self.states[self.current]) != list:
+			return
+
+		opts = self.states[self.current]
+		index = self.indexes[self.current]
+		if index == len(opts) - 1:
+			return
+
+		self.indexes[self.current] += 1
+		line_no = first_line + self.current
+		stdscr.addstr(line_no, 0, "- {}: {}".format(self.options[self.current], opts[index+1]), curses.A_BOLD)
+	#
+	def print_prev_element(self, stdscr, first_line=2):
+		if type(self.states[self.current]) != list:
+			return
+
+		opts = self.states[self.current]
+		index = self.indexes[self.current]
+		if index == 0:
+			return
+
+		self.indexes[self.current] -= 1
+		line_no = first_line + self.current
+		stdscr.addstr(line_no, 0, "- {}: {}".format(self.options[self.current], opts[index-1]), curses.A_BOLD)
+	#
+	def is_exit(self):
+		return False if self.current < len(self.options) - 1 else True
+	#
+#
 
 def scroll_list(stdscr, lists):
 	pass
 #
 
-
-def print_options(stdscr, options, whch, first_line=2):
-	i = first_line
-	bold_line = -1
-	bold_element = None
-
-	for option,elements in options.items():
-		if elements[0] == False:
-			continue
-		opts = elements[2]
-		index = elements[1]
-
-		if i == whch + first_line:
-			bold_element = option
-			bold_line = i
-
-		if index < len(opts):
-			stdscr.addstr(i, 0, "- {}: {}".format(option, opts[index]) )
-		else:
-			stdscr.addstr(i, 0, "- {}".format(option) )
-		i += 1
-
-	if bold_line >= first_line:
-		elements = options[bold_element]
-		opts = elements[2]
-		index = elements[1]
-
-		if index < len(opts):
-			stdscr.addstr(bold_line, 0, "- {}: {}".format(bold_element, opts[index]), curses.A_BOLD)
-		else:
-			stdscr.addstr(bold_line, 0, "- {}".format(bold_element), curses.A_BOLD)
-#
-
-def switch_element_option(options, whch, increment):
-	i = 0
-	element = None
-	for option,elements in options.items():
-		if elements[0] == False:
-			continue
-
-		
-		i += 1
-
-#
 
 def menu(stdscr, albums):
 	stdscr = curses.initscr()
@@ -67,38 +107,34 @@ def menu(stdscr, albums):
 	cmd = 0
 
 	stdscr.addstr(0, 4, "Music library.")
-	options = {"Get longest album": [True, 0, []],
-				"Get total albums length": [True, 0, []],
-				"Get genre stats": [True, 0, []],
-				"Get last oldest": [True, 0, []],
-				"Get last oldest of genre": [True, 0, []],
-				"Get albums by genre":[True, 0, []],
-				"Exit":[True, 0, []]}
-# TEST_QUESTIONS = 5
+	options = Options()
+	options.add("Get longest album", None)
+	options.add("Get total albums length", None)
+	options.add("Get genre stats", None)
+	options.add("Get last oldest", None)
+	options.add("Get last oldest of genre", None)
+	options.add("Get last oldest of genre", None)
 
 	genres = music_reports.get_genres(albums)
 	if len(genres) > 0:
-		options["Get albums by genre"] = [True, 0, genres]
-	else:
-		options["Get albums by genre"] = [False, 0, []]
+		options.add("Get albums by genre", genres)
 
 	while run:
-		print_options(stdscr, options, cmd)
-
+		options.scr_print(stdscr)
 
 		c=stdscr.getch()
 
-		if c==curses.KEY_UP and cmd > 0:
-			cmd -= 1
-		elif c==curses.KEY_DOWN and cmd < len(options):
-			cmd += 1
-		elif c==curses.KEY_RIGHT and i4genres < len(genres)-1 and cmd == TEST_QUESTIONS:
-			i4genres += 1
-		elif c==curses.KEY_LEFT and i4genres > 0 and cmd == TEST_QUESTIONS:
-			i4genres -= 1
-		elif (c==curses.KEY_ENTER or c==10) and cmd == 0:
-			pass
-		elif (c==curses.KEY_ENTER or c==10) and cmd == len(options):
+		if c==curses.KEY_UP:
+			options.move_up()
+		elif c==curses.KEY_DOWN:
+			options.move_down()
+		elif c==curses.KEY_RIGHT:
+			options.print_next_element(stdscr, 2)
+		elif c==curses.KEY_LEFT:
+			options.print_prev_element(stdscr, 2)
+		#elif (c==curses.KEY_ENTER or c==10) and cmd == 0:
+		#	pass
+		elif (c==curses.KEY_ENTER or c==10) and options.is_exit():
 			run = False
 
 	#
